64a53c84623d06907949d3872db745cf5d1d8dbf
han1eng <1872361816@qq.com>
2023-03-05 12:24:43 +0800

    Site updated: 2023-03-05 12:24:39

---
+++ js/funny_title.js
@@ -0,0 +1,16 @@
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "/funny.ico");
+        document.title = '╭(°A°`)╮ 怎么走了 ~';
+        clearTimeout(titleTime);
+    }
+    else {
+        $('[rel="icon"]').attr('href', "/favicon.ico");
+        document.title = '(ฅ>ω<*ฅ) 欢迎回来 ~' + OriginTitle;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
