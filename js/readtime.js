efef9dbc998d64aa1feed183de4124ac8ca6f357
han1eng <1872361816@qq.com>
2023-03-11 16:44:38 +0800

    Site updated: 2023-03-11 16:44:31

--- js/readtime.js
+++ js/readtime.js
@@ -1,20 +1,27 @@
-let oSpan = document.getElementsByTagName("readtime")[0];
-let localhostTime = new Date();                                 //获取页面打开的时间
-function tow(n) {
-    return n >= 0 && n < 10 ? '0' + n : '' + n;
+function pjax_reload() {
+    let oSpan = document.getElementsByTagName("readtime")[0];
+    let localhostTime = new Date();                                 //获取页面打开的时间
+    function tow(n) {
+        return n >= 0 && n < 10 ? '0' + n : '' + n;
+    }
+    setInterval(function () {
+        let goTime = new Date();                                    //获取动态时间
+        let diffTime = goTime.getTime() - localhostTime.getTime();
+        var second = Math.floor(diffTime / 1000);                   //未来时间距离现在的秒数
+        second = second % 86400;                                    //余数代表剩下的秒数；
+        var hour = Math.floor(second / 3600);                       //整数部分代表小时；
+        second %= 3600;                                             //余数代表 剩下的秒数；
+        var minute = Math.floor(second / 60);
+        second %= 60;
+        // var str = tow(day) + '<span class="time">天</span>'
+        var str = tow(hour) + '<span class="time">小时</span>'
+            + tow(minute) + '<span class="time">分钟</span>'
+            + tow(second) + '<span class="time">秒</span>';
+        oSpan.innerHTML = "您已浏览当前界面" + str;
+    }, 1000)
 }
-setInterval(function () {
-    let goTime = new Date();                                    //获取动态时间
-    let diffTime = goTime.getTime() - localhostTime.getTime();
-    var second = Math.floor(diffTime / 1000);                   //未来时间距离现在的秒数
-    second = second % 86400;                                    //余数代表剩下的秒数；
-    var hour = Math.floor(second / 3600);                       //整数部分代表小时；
-    second %= 3600;                                             //余数代表 剩下的秒数；
-    var minute = Math.floor(second / 60);
-    second %= 60;
-    // var str = tow(day) + '<span class="time">天</span>'
-    var str = tow(hour) + '<span class="time">小时</span>'
-        + tow(minute) + '<span class="time">分钟</span>'
-        + tow(second) + '<span class="time">秒</span>';
-    oSpan.innerHTML = "您已浏览当前界面" + str;
-}, 1000)
+
+// Pjax 完成后，重新加载上面的函数
+document.addEventListener("pjax:complete", function () {
+    pjax_reload();
+  });
